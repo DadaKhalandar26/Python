c007112a0275be797b76026f77597674502b67c0
Bandar Shaik Dada Khalandar <123884804+DadaKhalandar26@users.noreply.github.com>
2024-03-18 22:02:22 +0530

    Update Main.py
    Core Code for CoffeeMachine

--- Main.py
+++ Main.py
@@ -1,8 +1,11 @@
+import copy
+
 MENU = {
     "espresso": {
         "ingredients": {
             "water": 50,
             "coffee": 18,
+            "milk": 0
         },
         "cost": 1.5,
     },
@@ -31,3 +34,72 @@ resources = {
 }
 
 
+# TODO: 1. getting reports from the data base
+def report():
+    global profit
+    print(f"Water: {current_resources["water"]}ml\n"
+          f"Milk: {current_resources["milk"]}ml\n"
+          f"Coffee: {current_resources["coffee"]}g\n"
+          f"Money: ${profit}"
+          )
+
+
+# TODO 2. Calculating the total Money inserted by user
+def asking_and_calculating_money():
+    """ Function will ask user for input and add all money, Returns the values"""
+    print("Please insert coins.")
+    quarters = int(input("how many quarters?: "))
+    dimes = int(input("how many dimes?: "))
+    nickles = int(input("how many nickles?: "))
+    pennies = int(input("how many pennies?: "))
+    total = quarters * 0.25 + dimes * 0.10 + nickles * 0.05 + pennies * 0.01
+    return total
+
+
+# TODO 3. Function to check resources are sufficient or not
+def resources_checker(current_resources, ordered_coffee):
+    """This Function checks weather machine have sufficent number of resources"""
+    for items in MENU[ordered_coffee]["ingredients"]:
+        if current_resources[items] >= MENU[ordered_coffee]["ingredients"][items]:
+            current_resources[items] -= MENU[ordered_coffee]["ingredients"][items]
+        elif current_resources[items] < MENU[ordered_coffee]["ingredients"][items]:
+            print(f"Sorry there is not enough {items}.")
+            return 1
+
+
+# TODO 4. FUNCTION FOR calculating the change and money
+def money_checker(orders_coffee, total_money):
+    """This Function will calculate the return change and if insufficient money entered"""
+    global profit
+    if total_money > MENU[orders_coffee]["cost"]:
+        total_money -= MENU[orders_coffee]["cost"]
+        print(f"Here is ${round(total_money, ndigits=2)} in change.")
+        profit += MENU[orders_coffee]["cost"]
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return 0
+
+
+profit = 0
+current_resources = copy.copy(resources)
+CoffeeMachine = True
+while CoffeeMachine:
+    Ordered_Coffee = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    # Creating copy of the resources so that we can modify it accordingly
+
+    if Ordered_Coffee == "off":
+        CoffeeMachine = False
+        print("Powering off")
+        break
+    elif Ordered_Coffee == "report":
+        report()
+    elif Ordered_Coffee == "espresso" or Ordered_Coffee == "latte" or Ordered_Coffee == "cappuccino":
+        resources = resources_checker(current_resources, Ordered_Coffee)
+        if resources == 1:
+            continue
+        Total_Money = asking_and_calculating_money()
+        money = money_checker(Ordered_Coffee, Total_Money)
+        if money != 0:
+            print(f"Here is your {Ordered_Coffee} ☕️. Enjoy!")
+    else:
+        print("Enter the correct details.")
